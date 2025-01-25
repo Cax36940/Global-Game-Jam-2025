600e89d315be6f21481db5443faa9a376f54d475
Cax36940 <115898972+Cax36940@users.noreply.github.com>
2025-01-25 15:09:04 -0500

    Fix rock layer display

--- assets/rock_layer.gd
+++ assets/rock_layer.gd
@@ -11,14 +11,14 @@ var rock_color : Color = Color(1.0, 1.0, 1.0)
 var velocity : Vector2 = Vector2.ZERO
 var velocity_factor : float = 1.0 # factor to account for depth of the layer
 var y_offset : float = 0.0
-var begin_y : float = -100.0
+var begin_y : float = -200.0
 var end_y : float
-var width : float = 200.0
+var width : float = 2000.0
 
 var spacing : float = 20.0
+var init_global_position : Vector2 = Vector2.ZERO
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	velocity.y = 100.0
 	end_y  = get_viewport_rect().size.y + 100
 	
 	left_rock.set_color(rock_color)
@@ -39,8 +39,8 @@ func _ready() -> void:
 	
 	
 	# Fill left rock points
-	points.push_back(Vector2(0,end_y))
-	points.push_back(Vector2(0,begin_y))
+	points.push_back(Vector2(-100000,end_y))
+	points.push_back(Vector2(-100000,begin_y))
 	for i in range(begin_y, end_y + spacing, spacing):
 		points.push_back(Vector2(width + left_noise.get_noise_1d(i) * 20, i))
 	left_rock.polygon = points
@@ -48,13 +48,13 @@ func _ready() -> void:
 	points.clear()
 	
 	# Fill right rock points
-	points.push_back(Vector2(get_viewport_rect().size.x + 100,end_y))
-	points.push_back(Vector2(get_viewport_rect().size.x + 100,begin_y))
+	points.push_back(Vector2(get_viewport_rect().size.x + 100000,end_y))
+	points.push_back(Vector2(get_viewport_rect().size.x + 100000,begin_y))
 	for i in range(begin_y, end_y + spacing, spacing):
 		points.push_back(Vector2(get_viewport_rect().size.x - width + right_noise.get_noise_1d(i) * 20, i))
 		
 	right_rock.polygon = points
-	
+	init_global_position = global_position
 	pass # Replace with function body.
 
 
@@ -83,7 +83,7 @@ func update_left_rock(delta):
 		for i in range(number_points_to_change + 1, 1, -1):
 			y_offset -= spacing
 			number_added_point += 1
-			points[i] = Vector2(width + left_noise.get_noise_1d(begin_y + y_offset) * 20, points[number_points_to_change + 2].y - spacing * number_added_point)
+			points[i] = Vector2(width - (global_position.x - init_global_position.x) * velocity_factor + left_noise.get_noise_1d(begin_y + y_offset) * 20, points[number_points_to_change + 2].y - spacing * number_added_point)
 	left_rock.polygon = points
 
 func update_right_rock(delta):
@@ -105,7 +105,7 @@ func update_right_rock(delta):
 		for i in range(number_points_to_change + 1, 1, -1):
 			y_offset -= spacing
 			number_added_point += 1
-			points[i] = Vector2(get_viewport_rect().size.x - width + right_noise.get_noise_1d(begin_y + y_offset) * 20, points[number_points_to_change + 2].y - spacing * number_added_point)
+			points[i] = Vector2(get_viewport_rect().size.x - width - (global_position.x - init_global_position.x) * velocity_factor + right_noise.get_noise_1d(begin_y + y_offset) * 20, points[number_points_to_change + 2].y - spacing * number_added_point)
 	right_rock.polygon = points
 
 func set_width(new_width : float):
