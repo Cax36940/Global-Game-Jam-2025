85dc79a02dcd801f590d0521e756297a1298b6c1
tploquin <timothee.ploquin@enseirb-matmeca.fr>
2025-01-25 15:22:28 +0100

    add geyser

--- collectibles/bubblita.gd
+++ collectibles/bubblita.gd
@@ -1,6 +1,10 @@
 extends RigidBody2D
 
 
+var velocity = Vector2(0, -1.0)
+const HSPEED_VARIATION = 0.2
+const VSPEED_VARIATION = 0.2
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
@@ -8,4 +12,7 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	velocity.x += randfn(0.0, 0.2) * HSPEED_VARIATION
+	velocity.y += randfn(0.0, 0.1) * VSPEED_VARIATION
+	position.x += velocity.x
+	position.y += velocity.y
---
+++ collectibles/geyser.gd
@@ -0,0 +1,27 @@
+extends Node2D
+
+
+const BUBBLITA_PER_SEC = 4.0
+var bubblita_scence = preload("res://collectibles/bubblita.tscn")
+var initial_bubblita_mean_speed = Vector2(0, 0)
+
+func create_bubblita():
+	var bubblita = bubblita_scence.instantiate()
+	var vx = initial_bubblita_mean_speed.x + randfn(0.0, 0.1)
+	var vy = initial_bubblita_mean_speed.y + randfn(0.0, 0.1)
+	bubblita.velocity.x = vx
+	bubblita.velocity.y = vy
+	add_child(bubblita)
+
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	initial_bubblita_mean_speed.x = randf() * 2 - 1
+	initial_bubblita_mean_speed.y = - 2 - randf() * 2
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if randf() < delta * BUBBLITA_PER_SEC:
+		create_bubblita()
