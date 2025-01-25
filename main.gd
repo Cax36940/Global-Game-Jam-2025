a3d8f12797ce448936a5fe6be3c260f7546777ce
tploquin <timothee.ploquin@enseirb-matmeca.fr>
2025-01-25 10:19:40 +0100

    add rock bg to main scene

---
+++ main.gd
@@ -0,0 +1,12 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	$RockBackground.position.y = $Bubble.position.y - get_viewport_rect().size.y / 2
+	$RockBackground.position.x = $Bubble.position.x - get_viewport_rect().size.x / 2
