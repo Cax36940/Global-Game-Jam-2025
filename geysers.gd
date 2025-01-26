0e4120938e61d39043d289280b657d9af0eecd48
Cax36940 <115898972+Cax36940@users.noreply.github.com>
2025-01-26 13:06:30 -0500

    Fix warning

--- geysers.gd
+++ geysers.gd
@@ -19,8 +19,3 @@ func _ready() -> void:
 		position_y -= 500 * 50
 		
 	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
