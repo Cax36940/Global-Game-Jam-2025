3dda53d07f13779724ab4bf807542a0c6af010a3
Skydrag <juli1.charvet@gmail.com>
2025-01-26 15:33:19 +0100

    Added another stat

--- main.gd
+++ main.gd
@@ -3,13 +3,15 @@ extends Node2D
 
 
 var is_in_game = false
-
+var max_depth_reached = 0
+var total_distance_traveled = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Bubble.set_can_update(is_in_game)
 	$Bubble.bubble_died.connect(on_end_game)
-
+	max_depth_reached = $Bubble.MAX_DEPTH
+	$MainUI.update_stats(max_depth_reached, max_depth_reached, total_distance_traveled)
 
 func earn_h2o() -> void:
 	var dist = $Bubble.MAX_DEPTH - $Bubble.depth
@@ -18,6 +20,10 @@ func earn_h2o() -> void:
 
 
 func on_end_game() -> void:
+	if max_depth_reached > $Bubble.depth:
+		max_depth_reached = $Bubble.depth
+	total_distance_traveled += $Bubble.MAX_DEPTH - $Bubble.depth
+	$MainUI.update_stats(max_depth_reached, $Bubble.depth, total_distance_traveled)
 	$MainUI.show_main_ui()
 	is_in_game = false
 	earn_h2o()
