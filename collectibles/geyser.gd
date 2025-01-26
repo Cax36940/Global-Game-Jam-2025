2a8e6fb313ec53a9d33ba3df817a014fd6537b91
Cax36940 <115898972+Cax36940@users.noreply.github.com>
2025-01-26 10:23:27 -0500

    Add geyser sprite

--- collectibles/geyser.gd
+++ collectibles/geyser.gd
@@ -5,6 +5,9 @@ const BUBBLITA_PER_SEC = 2.0
 var bubblita_scene = preload("res://collectibles/bubblita.tscn")
 var initial_bubblita_mean_speed = Vector2(0, 0)
 
+var total_time : float = randf_range(0, 10)
+var initial_position : Vector2
+
 func create_bubblita():
 	var bubblita = bubblita_scene.instantiate()
 	var vx = initial_bubblita_mean_speed.x + randfn(0.0, 0.1)
@@ -18,6 +21,7 @@ func create_bubblita():
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	initial_position = position
 	initial_bubblita_mean_speed.x = randfn(0.0, 0.1)
 	initial_bubblita_mean_speed.y = randfn(-3, 0.1)
 	$AudioStreamPlayer2D.play()
@@ -27,3 +31,6 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	if randf() < delta * BUBBLITA_PER_SEC:
 		create_bubblita()
+		
+	total_time += delta
+	position.y = initial_position.y + 20 * sin(total_time)
